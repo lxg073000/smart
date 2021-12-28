183bb485e0c2249f23e1d9397d4b1fc8cdb40d28
App Academy Student <lxg073000@gmail.com>
2021-12-28 14:39:25 -0500

    create route guards and redirects for home, login, and signup pages dependent on AuthContext user value

--- src/hooks/useAuthContext.js
+++ src/hooks/useAuthContext.js
@@ -1,12 +1,13 @@
-import { AuthContext } from "../context/AuthContext"
-import { useContext } from "react"
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 
+// Custom Hook which returns the AuthContext object
 export const useAuthContext = () => {
-  const context = useContext(AuthContext)
+  const context = useContext(AuthContext);
 
-  if(!context) {
-    throw Error('useAuthContext must be used inside an AuthContextProvider')
+  if (!context) {
+    throw Error("useAuthContext must be used inside an AuthContextProvider");
   }
 
-  return context
-}
+  return context;
+};

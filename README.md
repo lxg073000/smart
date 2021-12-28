5e10c3053f8686926e315110533db8fe16b4bcab
App Academy Student <lxg073000@gmail.com>
2021-12-28 09:50:55 -0500

    connect Goal deletion to realtime database

--- README.md
+++ README.md
@@ -69,4 +69,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
 
-// Modular Realtime data using custom useCollection hook
+// Modular Realtime data using custom useCollection hook increases resuability
--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,14 +1,26 @@
+import trashcan from "../assets/trashcan.svg";
+import { db } from "../firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+
 export default function GoalList({ goals }) {
   const handleClick = async (id) => {
-    console.log(id);
+    const ref = doc(db, "users/demo/goals", id);
+    await deleteDoc(ref);
+    alert(id + " was deleted");
   };
 
   return (
     <div className="goal-list">
       <ul>
         {goals.map((goal) => (
-          <li key={goal.id} onClick={() => handleClick(goal.id)}>
-            {goal.title}
+          <li key={goal.id}>
+            <p>{goal.title}</p>
+            <img
+              src={trashcan}
+              className="material-icons-outlined"
+              aria-label="delete goal"
+              onClick={() => handleClick(goal.id)}
+            ></img>
           </li>
         ))}
       </ul>
